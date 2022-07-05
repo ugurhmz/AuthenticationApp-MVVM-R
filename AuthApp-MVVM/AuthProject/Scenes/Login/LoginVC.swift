f1d864ba368cda6da49365f50de1c6012961ae85
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-07-05 10:08:14 +0300

    structure creating

---
+++ AuthApp-MVVM/AuthProject/Scenes/Login/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthApp-MVVM
+//
+//  Created by ugur-pc on 4.07.2022.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
